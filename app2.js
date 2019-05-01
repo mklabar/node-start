64c8c91741c5f2cdb139692833134051aa2bd602
Mary Kate La Bar <labarm@spu.edu>
2019-04-30 20:24:25 -0700

    adjusted meta data, added implementation for app2

--- app2.js
+++ app2.js
@@ -2,7 +2,7 @@ const http = require('http');
 const hostname = 'leia.cs.spu.edu';
 const port = 3015;
 
-var friends = require("./friends.json"); // Once for all times
+var friends = require("./friends2.json"); // Once for all times
 
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
@@ -16,13 +16,13 @@ const server = http.createServer((request, response) => {
         '               <meta name="viewport" content="width=device-width, initial-scale=1"> \n' +
         '               <meta name="description" content="Home Page"> \n' +
         '               <meta name="author" content="Mary Kate La Bar"> \n' +
-        '               <title>Node Example</title> \n' +
+        '               <title>Node Example 2</title> \n' +
         '               <!-- Bootstrap core CSS --> \n' +
 		'				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">\n' +
         '       </head> \n' +
         '        <body> \n' +
         '               <div class="container" style="text-align: center"> \n' +
-        '               <h1>Another Example of Node</h1><br> \n'
+        '               <h1>Another (Another) Example of Node</h1><br> \n'
 
     );
 	var currentDate = new Date();
@@ -30,25 +30,52 @@ const server = http.createServer((request, response) => {
         '               <p>Current time is: ' + currentDate + '</p>'
     );
     response.write(
+        '               <div class="container" style="text-align: left"> \n' +
+        '               <h2></h2>Boys<br> \n' +
         '               <table class="table table-bordered table-hover"> \n' +
         '                       <thead> \n' +
         '                               <tr> \n' +
         '                                       <th scope="col">First Name</th> \n' +
         '                                       <th scope="col">Last Name</th> \n' +
         '                                       <th scope="col">Phone</th> \n' +
-        '                                       <th scope="col">Gender</th> \n' +
         '                               </tr> \n' +
         '                       </thead> \n' +
         '                       <tbody> \n'
     );
-	for (var key in friends)
-	        for (var f in friends[key])
+	for (var key in boys)
+	        for (var f in boys[key])
 	            response.write(
 	                '                               <tr> \n' +
-	                '                                       <td>' + friends[key][f]["firstName"] + '</td> \n' +
-	                '                                       <td>' + friends[key][f]["lastName"] + '</td> \n' +
-	                '                                       <td>' + friends[key][f]["phone"] + '</td> \n' +
-	                '                                       <td>' + friends[key][f]["gender"] + '</td> \n' +
+	                '                                       <td>' + boys[key][f]["firstName"] + '</td> \n' +
+	                '                                       <td>' + boys[key][f]["lastName"] + '</td> \n' +
+	                '                                       <td>' + boys[key][f]["phone"] + '</td> \n' +
+	                '                               </tr> \n'
+	            );
+
+	    response.write(
+	        '                       </tbody> \n' +
+	        '               </table> \n'
+	    );
+    response.write(
+        '               <div class="container" style="text-align: left"> \n' +
+        '               <h2></h2>Boys<br> \n' +
+        '               <table class="table table-bordered table-hover"> \n' +
+        '                       <thead> \n' +
+        '                               <tr> \n' +
+        '                                       <th scope="col">First Name</th> \n' +
+        '                                       <th scope="col">Last Name</th> \n' +
+        '                                       <th scope="col">Phone</th> \n' +
+        '                               </tr> \n' +
+        '                       </thead> \n' +
+        '                       <tbody> \n'
+    );
+	for (var key in girls)
+	        for (var f in girls[key])
+	            response.write(
+	                '                               <tr> \n' +
+	                '                                       <td>' + girls[key][f]["firstName"] + '</td> \n' +
+	                '                                       <td>' + girls[key][f]["lastName"] + '</td> \n' +
+	                '                                       <td>' + girls[key][f]["phone"] + '</td> \n' +
 	                '                               </tr> \n'
 	            );
 
